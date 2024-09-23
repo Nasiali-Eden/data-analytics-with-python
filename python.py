1867b4e61484869b6d36d4d9c2ee2adf448e3475
nasiali-eden <nasieden0@gmail.com>
2024-09-23 08:41:58 +0300

    Run

--- python.py
+++ python.py
@@ -4,11 +4,14 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-import os
-for dirname, _, filenames in os.walk('/kagggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
+# import os
+# for dirname, _, filenames in os.walk('/kagggle/input'):
+#     for filename in filenames:
+#         print(os.path.join(dirname, filename))
 
 
 #DATA INPUT
-income_df = pd.read_csv('')
+income_df = pd.read_csv('Inc_Exp_Data.csv')
+
+# Display the first few rows of the dataframe
+print(income_df.head())
