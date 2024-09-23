3c5271e8a4c622e214e2486500028ee52e4a3d98
nasiali-eden <nasieden0@gmail.com>
2024-09-23 08:47:49 +0300

    Printed all columns horizontally

--- python.py
+++ python.py
@@ -4,6 +4,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Set pandas to display all columns in one line (horizontally)
+pd.set_option('display.width', 1000)  # Set a high enough value for width
+
 # import os
 # for dirname, _, filenames in os.walk('/kagggle/input'):
 #     for filename in filenames:
@@ -13,5 +16,11 @@ import matplotlib.pyplot as plt
 #DATA INPUT
 income_df = pd.read_csv('Inc_Exp_Data.csv')
 
-# Display the first few rows of the dataframe
+# Show all columns in the DataFrame
+pd.set_option('display.max_columns', None)
+
+# If you want to display all rows as well, you can set this option too
+pd.set_option('display.max_rows', None)
+
+# Now, when you print the DataFrame, it will show all columns
 print(income_df.head())
